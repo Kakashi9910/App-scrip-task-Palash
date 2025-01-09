cef5a05f88581c3c9bdd2bcdcad26826a197bb8e
Palash <palashsharma359@gmail.com>
2025-01-09 11:15:04 +0530

    removed unused imports

--- app/component/filters/Filters.tsx
+++ app/component/filters/Filters.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import styles from './Filters.module.css'
 import { useState } from 'react';
 
