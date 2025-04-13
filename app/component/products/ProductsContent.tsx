dd25fa91cdfe84cda761d975e9b43bf1452e43f8
Palash <palashsharma359@gmail.com>
2025-04-13 19:13:26 +0530

    register domain for image

--- app/component/products/ProductsContent.tsx
+++ app/component/products/ProductsContent.tsx
@@ -1,14 +1,20 @@
+import Image from 'next/image'
 import styles from './ProductContent.module.css'
 
-export default  function Products ({hideFilter,productData}:{hideFilter:boolean,productData:any}) {
-    const style = hideFilter?styles.gridContainerHideFilter:styles.gridContainer
+export default function Products({ hideFilter, productData }: { hideFilter: boolean, productData: any }) {
+    const style = hideFilter ? styles.gridContainerHideFilter : styles.gridContainer
 
-    // console.log(productData)
 
     return <div className={style}>
-        {productData.map((product:any,index:number)=>(
+        {productData.map((product: any, index: number) => (
             <div key={index} >
-                <img style={{width:'80%',height:'80%'}} src={product.image} alt="" />
+                <Image
+                    src={product.image}
+                    alt="product"
+                    width={500}  
+                    height={500} 
+                    style={{ width: '80%', height: 'auto' }}
+                />
                 <div>
                     <p className={styles.title}>{product.title}</p>
                     <p>Sign in in or create an account an account to see pricing</p>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fakestoreapi.com'], // 👈 Add allowed image domains here
+  },
 };
 
 export default nextConfig;
